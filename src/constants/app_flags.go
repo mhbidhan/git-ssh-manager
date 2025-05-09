913faa854c45bfa57cd135562054875d5856f0d2
mhbidhan <mhbidhan5@gmail.com>
2025-05-09 15:32:12 +0600

    Defined flags for application commands.

---
+++ src/constants/app_flags.go
@@ -0,0 +1,14 @@
+package constants
+
+var AppFlags = map[string]string{
+	"-s":       "--status",
+	"--status": "--status",
+	"-n":       "--new",
+	"--new":    "--new",
+	"-u":       "--use",
+	"--use":    "--use",
+	"-r":       "--remove",
+	"--remove": "--remove",
+	"-h":       "--help",
+	"--help":   "--help",
+}
