d8c177dae50e0391ddbb84b55a3a754862563ff7
mhbidhan <mhbidhan5@gmail.com>
2025-05-09 15:32:51 +0600

    Defined a constant describing the application commands.

---
+++ src/constants/commands.go
@@ -0,0 +1,10 @@
+package constants
+
+const Commands = `
+-s, --status		show active profile
+-n, --new		create new profile
+-u, --use		use to profile
+-r, --remove		remove profile
+-h, --help		list all commands
+
+`
