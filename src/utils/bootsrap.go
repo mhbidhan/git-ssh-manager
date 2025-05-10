c0fef9844f044b0d5d6d3bf15d687d46c90fb6aa
mhbidhan <mhbidhan5@gmail.com>
2025-05-10 06:11:27 +0600

    Defined utility function to bootstrap the application.

---
+++ src/utils/bootsrap.go
@@ -0,0 +1,47 @@
+package utils
+
+import (
+	"os"
+
+	"github.com/mhbidhan/github-ssh-manager/src/interfaces"
+)
+
+func Bootstrap(githubSshManager interfaces.GithubSshManager) {
+	fileManager := FileManager{}
+	filepath := githubSshManager.GetFilePaths()
+
+	err := fileManager.CreateDir(filepath.RootDir)
+
+	if err != nil {
+		PrintErrorMessage(err.Error())
+	}
+
+	err = fileManager.CreateDir(filepath.SSHDirPath)
+
+	if err != nil {
+		PrintErrorMessage(err.Error())
+	}
+	var profileFile any
+
+	profileFile, err = os.Stat(filepath.ProfileFilePath)
+
+	if profileFile == nil {
+		err = fileManager.WriteFile(filepath.ProfileFilePath, nil)
+
+		if err != nil {
+			PrintErrorMessage(err.Error())
+		}
+	}
+
+	var activeProfileFile any
+
+	activeProfileFile, err = os.Stat(filepath.ActiveProfileFilePath)
+
+	if activeProfileFile == nil {
+		err = fileManager.WriteFile(filepath.ActiveProfileFilePath, nil)
+
+		if err != nil {
+			PrintErrorMessage(err.Error())
+		}
+	}
+}
