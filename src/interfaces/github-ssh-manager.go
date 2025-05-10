090ee1e93793b1b4f2eb2171da6f3741f511ec03
mhbidhan <mhbidhan5@gmail.com>
2025-05-10 06:08:50 +0600

    Defined interface for GithubSshManager.

---
+++ src/interfaces/github-ssh-manager.go
@@ -0,0 +1,12 @@
+package interfaces
+
+import models_file_paths "github.com/mhbidhan/github-ssh-manager/src/models/file_paths"
+
+type GithubSshManager interface {
+	GetFilePaths() models_file_paths.FilePaths
+
+	Status() error
+	New(profileName string) error
+	Use(profileName string) error
+	Remove(profileName string) error
+}
