1dbed8c82b48b3cbe0b1aad804d32f24985a486c
mhbidhan <mhbidhan5@gmail.com>
2025-05-10 06:12:50 +0600

    Defined status function.

---
+++ src/app/github_ssh_manager/status.go
@@ -0,0 +1,28 @@
+package github_ssh_manager
+
+import (
+	"fmt"
+
+	"github.com/mgutz/ansi"
+	models_file_paths "github.com/mhbidhan/github-ssh-manager/src/models/file_paths"
+	models_profile "github.com/mhbidhan/github-ssh-manager/src/models/profile"
+)
+
+func Status(filePaths models_file_paths.FilePaths) error {
+	profiles, err := models_profile.GetProfiles(filePaths.ProfileFilePath)
+	activeProfile, err := models_profile.GetActiveProfile(filePaths.ActiveProfileFilePath, profiles)
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Print(ansi.ColorCode("green+h:black"), "\n"+activeProfile.ProfileName, ansi.ColorCode("reset"))
+
+	fmt.Printf(`
+------------------------
+Username: %s
+Email: %s
+`, activeProfile.Username, activeProfile.Email)
+
+	return nil
+}
