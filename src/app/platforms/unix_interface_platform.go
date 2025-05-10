f1e9d8f313955acddf4abd26efd8ee9921d8b29d
mhbidhan <mhbidhan5@gmail.com>
2025-05-10 06:14:48 +0600

    Defined app for unix interface systems.

---
+++ src/app/platforms/unix_interface_platform.go
@@ -0,0 +1,30 @@
+package platforms
+
+import (
+	"github.com/mhbidhan/github-ssh-manager/src/app/github_ssh_manager"
+	models_file_paths "github.com/mhbidhan/github-ssh-manager/src/models/file_paths"
+)
+
+type UnixInterfacePlatform struct {
+	models_file_paths.FilePaths
+}
+
+func (app UnixInterfacePlatform) GetFilePaths() models_file_paths.FilePaths {
+	return app.FilePaths
+}
+
+func (app UnixInterfacePlatform) Status() error {
+	return github_ssh_manager.Status(app.FilePaths)
+}
+
+func (app UnixInterfacePlatform) New(profileName string) error {
+	return github_ssh_manager.New(app.FilePaths, profileName)
+}
+
+func (app UnixInterfacePlatform) Use(profileName string) error {
+	return github_ssh_manager.Use(app.FilePaths, profileName)
+}
+
+func (app UnixInterfacePlatform) Remove(profileName string) error {
+	return github_ssh_manager.Remove(app.FilePaths, profileName)
+}
