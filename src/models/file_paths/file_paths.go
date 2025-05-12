35cac760ada2daf5d4796b08e7f3dd832ab07e63
mhbidhan <mhbidhan5@gmail.com>
2025-05-12 06:23:56 +0600

    Modified file paths to work cross platform

--- src/models/file_paths/file_paths.go
+++ src/models/file_paths/file_paths.go
@@ -1,5 +1,7 @@
 package models_file_paths
 
+import "path/filepath"
+
 type FilePaths struct {
 	HomeDir               string
 	RootDir               string
@@ -12,9 +14,9 @@ func GenerateFilePaths(homeDir string, rootDir string) FilePaths {
 	return FilePaths{
 
 		HomeDir:               homeDir,
-		RootDir:               homeDir + rootDir,
-		ProfileFilePath:       homeDir + rootDir + "/profiles.json",
-		SSHDirPath:            homeDir + rootDir + "/ssh_keys",
-		ActiveProfileFilePath: homeDir + rootDir + "/active-profile.txt",
+		RootDir:               filepath.Join(homeDir, rootDir),
+		ProfileFilePath:       filepath.Join(homeDir, rootDir, "/profiles.json"),
+		SSHDirPath:            filepath.Join(homeDir, rootDir, "/ssh_keys"),
+		ActiveProfileFilePath: filepath.Join(homeDir, rootDir, "/active-profile.txt"),
 	}
 }
