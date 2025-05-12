a8a35ae390146f56c5716ac3a6aec7de69fab55b
mhbidhan <mhbidhan5@gmail.com>
2025-05-12 06:57:22 +0600

    Defined utility functions to copy and rename files

--- src/utils/file_manager.go
+++ src/utils/file_manager.go
@@ -2,7 +2,10 @@ package utils
 
 import (
 	"encoding/json"
+	"fmt"
+	"io"
 	"os"
+	"path/filepath"
 )
 
 type FileManager struct {
@@ -44,4 +47,58 @@ func (FileManager) CreateDir(path string) error {
 
 	return nil
 
+}
+
+func (FileManager) CopyFileToDirectory(srcFile, destinationDirectory string) error {
+	fileName := filepath.Base(srcFile)
+	destinationFile := filepath.Join(destinationDirectory, fileName)
+
+	source, err := os.Open(srcFile)
+
+	if err != nil {
+		return err
+	}
+	defer source.Close()
+
+	destination, err := os.Create(destinationFile)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if closeErr := destination.Close(); closeErr != nil && err == nil {
+			err = fmt.Errorf("failed to close destination file %s: %w", destinationFile, closeErr)
+		}
+	}()
+
+	_, err = io.Copy(destination, source)
+	if err != nil {
+		os.Remove(destinationFile)
+		return err
+	}
+
+	sourceStat, err := source.Stat()
+	if err != nil {
+		return err
+	} else {
+		err = os.Chmod(destinationFile, sourceStat.Mode())
+		if err != nil {
+			return err
+		}
+	}
+
+	return err
+}
+
+func (FileManager) RenameFile(filePath string, newFileName string) error {
+	dir := filepath.Dir(filePath)
+
+	newPath := filepath.Join(dir, newFileName)
+
+	err := os.Rename(filePath, newPath)
+	if err != nil {
+		return fmt.Errorf("failed to rename file from %s to %s: %w", filePath, newPath, err)
+	}
+
+	return nil
 }
